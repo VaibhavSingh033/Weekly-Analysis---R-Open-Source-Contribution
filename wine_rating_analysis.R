b933784c976b8c13d6607cee12ae1a98cfd2e79c
rockmama <singh.vaibhav033@gmail.com>
2019-06-23 15:22:41 +0530

    First Commit

--- wine_rating_analysis.R
+++ wine_rating_analysis.R
@@ -1,1 +1,18 @@
+
+
 wine_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-28/winemag-data-130k-v2.csv")
+
+#Exploring the file
+head(wine_ratings)
+dim(wine_ratings)
+
+
+#Initial Analysis
+
+
+wine_ratings %>% count(country) %>% 
+  fct_reorder(country,n)
+
+
+ggplot(wine_ratings, aes(country))+geom_bar()
+                         
