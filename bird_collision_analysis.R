6cfcbac63ef56c3a0f8a209a59b810fc71d38934
Vaibhav Singh <19756890+VaibhavSingh033@users.noreply.github.com>
2019-06-28 18:37:21 +0530

    Delete bird_collision_analysis.R

--- bird_collision_analysis.R
+++
@@ -1,2 +0,0 @@
-bird_collisions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/bird_collisions.csv")
-mp_light <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/mp_light.csv")
