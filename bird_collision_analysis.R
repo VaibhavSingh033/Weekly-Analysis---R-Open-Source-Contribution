1f454461d4a93b3743f18abe9c8b337bd0808a90
rockmama <singh.vaibhav033@gmail.com>
2019-06-28 17:40:16 +0530

    Analysing Bird collision

---
+++ bird_collision_analysis.R
@@ -0,0 +1,2 @@
+bird_collisions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/bird_collisions.csv")
+mp_light <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/mp_light.csv")
